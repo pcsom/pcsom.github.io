2bbb2ef43e3d2e44868a757da12a72359d62959c
Pranav Somu <pranavcsomu@gmail.com>
2025-09-17 15:58:02 -0400

    Update aboutMe with contact info

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
     github: "https://github.com/pcsom",
   },
   aboutMe:
-    "I'm pursuing my BS/MS degree in Computer Science at Georgia Tech, with a passion for AI/ML theory and systems. I'm interested in Neural Architecture Search and meta-algorithms that improve Machine Learning processes. I'm currently researching LLM- & surrogate-guided Evolutionary Algorithms to optimize computer vision models at the Georgia Tech Research Institute and Automated Algorithm Design team. I've also had the opportunity to build Gen-AI applications and optimize infrastructure at Amazon Web Services. I'm always eager to explore new approaches and applications in AI and Machine Learning algorithms. Let's connect!",
+    "I'm pursuing my BS/MS degree in Computer Science at Georgia Tech, with a passion for AI/ML theory and systems. I'm interested in Neural Architecture Search and meta-algorithms that improve Machine Learning processes. I'm currently researching LLM- & surrogate-guided Evolutionary Algorithms to optimize computer vision models at the Georgia Tech Research Institute and Automated Algorithm Design team. I've also had the opportunity to build Gen-AI applications and optimize infrastructure at Amazon Web Services. I'm always eager to explore new approaches and applications in AI and Machine Learning algorithms. Let's connect! The best way to reach me is via LinkedIn or email.",
   skills: ["Python", "C++", "Java", "SQL", "AWS"],
   projects: [
     {
