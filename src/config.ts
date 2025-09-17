88660b3dd0a9fdc3c6608758904fd886734f0090
Pranav Somu <pranavcsomu@gmail.com>
2025-09-17 16:17:57 -0400

    Revise project details

--- src/config.ts
+++ src/config.ts
@@ -14,39 +14,46 @@ export const siteConfig = {
   skills: ["Python", "C++", "Java", "SQL", "AWS"],
   projects: [
     {
+      name: "Surrogate-Aided Neural Architecture Search - Subteam Lead @ Georgia Tech Automated Algorithm Design Team",
+      description:
+        "Leading the development of a novel evolutionary algorithm for generating computer vision models. Built a deep-learning surrogate for predicting model performance, and integrated the predictor to decrease overall algorithm runtime by 27%.",
+      link: "https://github.com/AaronMcDaniel/surrogate-evolution",
+      skills: ["Python", "PyTorch", "Pandas", "NumpPy", "MatPlotLib"],
+    },
+    {
       name: "PhysioFix - GenAI Physiotherapy Trainer",
       description:
         "Developed a GenAI physiotherapy trainer as a Swift app, automating personalized treatment planning using a LangChain RAG pipeline with GPT-4o. A deployed text-to-motion model converts these plans into animated GIFs, offering accessible and clear visual guidance for exercises.",
       link: "https://devpost.com/software/physioflix-interactive-genai-physiotherapy-movement-trainer",
-      skills: ["LangChain", "Python", "Flask", "Swift"],
+      skills: ["LangChain", "Flask", "Swift", "Python"],
     },
     {
       name: "Agricultural Productivity Forecasting - Project Lead @ Big Data Big Impact",
       description:
         "Directed a team of 18 in creating a service to support data-driven agricultural policy by forecasting crop type and yield using satellite data. Led the optimization of LSTM and UNet models to achieve 91%+ prediction accuracy.",
-      link: "",
-      skills: ["Python", "AWS", "Pandas"],
+      link: "https://docs.google.com/presentation/d/1YxSpD4e0Cl23D1ED1i-8W_SxyTv06yqBzpCBEqVu5Ug",
+      skills: ["AWS", "Pandas", "Python"],
     },
     {
       name: "North Alabama Foster Closet",
       description:
         "Led the development of a full-stack supply distribution system for a non-profit, enabling the efficient completion of over 800 yearly requests for 300 foster families. Improved system performance and data query speeds by 220x by refining the Python backend, Django REST APIs, and PostgreSQL database.",
       link: "https://github.com/pcsom/foster-db-system",
-      skills: ["Python", "Django", "PostgreSQL", "JavaScript"],
+      skills: ["Django", "PostgreSQL", "JavaScript", "Python"],
     },
     {
       name: "American Sign Language (ASL) Education Tool - Big Data Big Impact",
       description:
         "Engineered deep learning algorithms for accessible ASL education through a real-time, ASL-to-English translation system. The solution uses a 95% accurate SSD ResNet50 hand sign classifier and a transformer architecture that surpassed prior research accuracy by 2%.",
       link: "https://github.com/pcsom/fingerspelling-zipformer-hybrid",
-      skills: ["Python", "PyTorch", "Pandas", "TensorFlow"],
+      skills: ["TensorFlow", "Docker", "PyTorch", "Python"],
     },
     {
       name: "NLP Restaurant Search",
       description:
         "Developed an intelligent restaurant search and recommender engine featuring simple filtering and demand analytics for businesses. Fine-tuned a BERT model to classify attributes like cuisine from plaintext searches, achieving 93% accuracy in understanding user intent.",
       link: "https://devpost.com/software/dishfish",
-      skills: ["Python", "Next.js", "Node.js", "MongoDB"],
+      skills: ["Next.js", "Node.js", "MongoDB", "Python"],
     },
   ],
   experience: [
@@ -70,14 +77,6 @@ export const siteConfig = {
       dateRange: "Jan. 2025 – April 2025",
       bullets: [
         "Improved an LLM-guided evolutionary algorithm to enhance YOLO object detection models through neural architecture search. Cut runtime for model refinement and training by 22% by running LLM inference through a central server with a ZeroMQ message queue.",
-      ],
-    },
-    {
-      company: "Georgia Tech Automated Algorithm Design Team",
-      title: "Undergraduate Researcher",
-      dateRange: "Oct. 2024 – Present",
-      bullets: [
-        "Leading the development of a novel evolutionary algorithm for generating computer vision models. Built a deep-learning surrogate for predicting model performance, and integrated the predictor to decrease overall algorithm runtime by 27%. https://github.com/AaronMcDaniel/surrogate-evolution [link](https://github.com/AaronMcDaniel/surrogate-evolution)",
       ],
     },
     {
